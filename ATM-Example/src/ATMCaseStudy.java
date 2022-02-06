08103bab48cf5597a6ef41df7f7fe6b66ae1121f
Jesse Kuehl <jkuehl0409@gmail.com>
2022-02-06 12:44:16 -0600

    adding example ATM project

---
+++ ATM-Example/src/ATMCaseStudy.java
@@ -0,0 +1,10 @@
+// Fig. 34.24: ATMCaseStudy.java
+// Driver program for the atm case study
+
+public class ATMCaseStudy {
+	// main method creates and runs the ATM
+	public static void main(String[] args) {
+		ATM theATM = new ATM();
+		theATM.run();
+	}
+}
