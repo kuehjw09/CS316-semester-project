aa9bf89af6c04cc8b6a7e2f4bdac2fe56bc3ad29
Jesse Kuehl <jkuehl0409@gmail.com>
2022-03-28 14:30:58 -0500

    alert/dialog box testing

--- FinalProject/src/FooterController.java
+++ FinalProject/src/FooterController.java
@@ -19,7 +19,7 @@ public class FooterController {
     void depositButtonPressed(ActionEvent event) {
     	System.out.println("deposit/withdrawal button pressed");
     	Alert alert = new Alert(AlertType.INFORMATION);
-    	alert.setTitle("Test Alert");
+    	alert.setTitle("Info Alert Test");
     	alert.setHeaderText("deposit/withdrawal button pressed");
     	alert.setContentText(currentUserSession.toString());
     	
@@ -29,18 +29,34 @@ public class FooterController {
     @FXML
     void homeButtonPressed(ActionEvent event) {
     	System.out.println("home button pressed");
-
+    	Alert alert = new Alert(AlertType.WARNING);
+    	alert.setTitle("Warning Alert Test");
+    	alert.setHeaderText("home button pressed");
+    	alert.setContentText(currentUserSession.toString());
+    	
+    	alert.showAndWait();
     }
 
     @FXML
     void menuButtonPressed(ActionEvent event) {
     	System.out.println("menu button pressed");
-
+    	Alert alert = new Alert(AlertType.CONFIRMATION);
+    	alert.setTitle("Confirmation Alert Test");
+    	alert.setHeaderText("menu button pressed");
+    	alert.setContentText(currentUserSession.toString());
+    	
+    	alert.showAndWait();
     }
 
     @FXML
     void transferButtonPressed(ActionEvent event) {
     	System.out.println("pay and transfer pressed");
+    	Alert alert = new Alert(AlertType.ERROR);
+    	alert.setTitle("Error Alert Test");
+    	alert.setHeaderText("pay and transfer button pressed");
+    	alert.setContentText(currentUserSession.toString());
+    	
+    	alert.showAndWait();
 
     }
 
