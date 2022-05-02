2da18ebd5696ce73c981308040a1d90b0c2aed6a
Jesse Kuehl <jkuehl0409@gmail.com>
2022-05-02 17:23:43 -0500

    changes to AddAccountsView

--- FinalProject/src/views/AddAccountPageController.java
+++ FinalProject/src/views/AddAccountPageController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
@@ -80,6 +81,7 @@ public class AddAccountPageController {
 				alert.close();
 			} else if (option.get() == ButtonType.OK) {
 				currentUserSession.addNewAccount(accountNameTextField.getText(), accountTypeChoicebox.getValue());
+	
 				returnButtonPressed();
 			} else {
 			alert.close();
